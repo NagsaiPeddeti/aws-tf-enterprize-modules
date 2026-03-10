395eeb0eb91e40f57e1325555ea77942bbee33b4
NagsaiPeddeti <nagsai.peddeti@gmail.com>
2026-03-10 17:36:56 +0000

    vpc module added

---
+++ modules/vpc/main.tf
@@ -0,0 +1,27 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "prod-enterprise-vpc"
+  cidr = var.vpc_cidr
+  azs  = var.vpc_azs
+
+  # 8 Private Subnets (4 depts * 2 AZs)
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  # This applies these tags to ALL private subnets
+  # To make them unique, we often use the 'index' approach or 
+  # logical naming in the 'Name' tag via module variables.
+  private_subnet_tags = {
+    "Layer" = "private"
+    "Environment" = var.label_private_subnet_tag_env
+  }
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "vpc_azs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b"]
+}
+
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+}
+
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "label_private_subnet_tag_env" {
+  description = "ENV variable to tag the private subnet"
+  type        = string
+  default     = "Prod"
+}
